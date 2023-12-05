bb18dbf24ff3975de9b470958c3bd70cfa944c06
Ian Meikle <ianmeikle@netpremacy.com>
2023-12-05 17:54:54 +0000

    Solutiuon to Day 4, part 1

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,13 +2,23 @@ module Day04 where
 
 import Paths_aoc2023 (getDataFileName)
 import Data.List.Split
+import qualified Data.Set as S
 
 getNumbers :: String -> [Int]
 getNumbers s = map read (words s)
 
+commonNums :: [[Int]] -> S.Set Int
+commonNums ss = S.fromList (head ss) `S.intersection` S.fromList  (last ss)
+
+score :: Int -> Int
+score 0 = 0
+score n = 2 ^ (n - 1)
+
 day04 :: IO ()
 day04 = do
-  inputLines <- lines <$> (getDataFileName "day04-input-test.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines $ map (unlines . splitOneOf "|:") inputLines
-  putStrLn "TODO: implement Day 04"
+  inputLines <- lines <$> (getDataFileName "day04-input.txt" >>= readFile)
+  -- putStrLn "This is what I read from input:"
+  -- putStrLn $ unlines $ map (unlines . splitOneOf "|:") inputLines
+  let cards = map (tail . splitOneOf "|:") inputLines
+  let cardSets = map (map getNumbers) cards
+  print $ sum $ map (score . S.size . commonNums) cardSets
